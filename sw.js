dd427adc41e09c423bba991e70d1458cc7d6fa81
James O'Sullivan <121300535+jamesdanedu@users.noreply.github.com>
2025-08-24 09:30:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
+// sw.js
+const CACHE_NAME = 'hazel-v2';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
